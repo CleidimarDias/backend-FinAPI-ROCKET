45d1aa69ac83ea1f7eed924cb015f75643bd7f25
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-05-23 13:21:44 -0300

    criado requisito realizar depósito com a regra não buscar extrato em conta que não existe

--- src/index.js
+++ src/index.js
@@ -4,14 +4,29 @@ const {v4: uuidv4} = require('uuid')
 const app = express();
 app.use(express.json())
 
-const customers = [];
-
 /**
  * cpf - string
  * name - string
  * id - uuid
  * statement []
  */
+const customers = [];
+
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers;
+    const customer = customers.find((customers)=>customers.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({erro: "customer not found"})
+    }
+
+    request.customer = customer; //respansndo o customer para as demais rotas
+    
+    return next();
+}
+
 
 app.post('/account', (request, response)=>{
 
@@ -20,11 +35,9 @@ app.post('/account', (request, response)=>{
     const customersAlreadyExists = customers.some((customers)=>customers.cpf === cpf);
 
     if (customersAlreadyExists){
-        console.log(cpf, name);
+       
         return response.status(400).json({error: "Customer already exists!"})
-    }
-
-    
+    }    
 
     customers.push({
         cpf,
@@ -36,18 +49,31 @@ app.post('/account', (request, response)=>{
     return response.status(201).send()
 })
 
-app.get("/statement", (request, response)=>{
-    const {cpf} = request.headers;
+// app.use(verifyIfExistsAccountCPF) quando precisar que todas as rotas contenha esse middleware
 
-    const customer = customers.find((customers) => customers.cpf === cpf);
-
-    if(!customer){
-        return response.status(400).json({error: "Customer not found"});
-    }
-
+app.get("/statement",verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+    
     return response.json(customer.statement);
 
 });
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+
+    const { description, amount} = request.body;
+
+    const statementOperation = {  //Operação para o depósito e saque
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation); // passando a operação para o statement de customer
+
+    return response.status(200).send();
+})
 
 
 app.listen(3333, ()=>{
